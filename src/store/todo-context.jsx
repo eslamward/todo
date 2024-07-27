a9fd61a07217ba443630d6b2a2c3afb492e3b3c9
eslam <eslamward000@gmail.com>
2024-07-27 15:27:25 +0300

    replace useState useReducer to manage state

--- src/store/todo-context.jsx
+++ src/store/todo-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 
 export const TodoContextState = createContext(
@@ -10,87 +10,114 @@ export const TodoContextState = createContext(
 )
 
 
+function todoReducerFunction(state, action) {
+    if (action.type === "START_ADD_TODO") {
+
+        return {
+            ...state,
+            todoId: null
+        };
+    } else if (action.type === "ADD_TODO") {
+        const id = Math.random();
+        const newTodo = {
+            ...action.payload,
+            todoId: id,
+            steps: []
+        };
+        return {
+            ...state,
+            todoId: undefined,
+            todos: [...state.todos, newTodo]
+        };
+    } else if (action.type === "CANCEL_TODO") {
+        return {
+            ...state,
+            todoId: undefined
+        };
+    } else if (action.type === "SELECTED_TODO") {
+        return {
+            ...state,
+            todoId: action.payload
+        }
+    } else if (action.type === "ADD_STEP") {
+        let newTodos = [...state.todos]
+        const todo = newTodos.find(todo => todo.todoId === state.todoId)
+        const updatedTodo = { ...todo, steps: [...todo.steps, action.payload] }
+        newTodos = newTodos.filter(todo => todo.todoId !== state.todoId)
+
+        return {
+            ...state,
+            todos: [...newTodos, updatedTodo]
+        }
+    } else if (action.type === "DELETE_STEP") {
+        let newTodos = [...state.todos]
+        const todo = newTodos.find(todo => todo.todoId === state.todoId)
+        const newSteps = todo.steps.filter(step => step.stepId !== action.payload)
+        const updatedTodo = { ...todo, steps: newSteps }
+        newTodos = newTodos.filter(todo => todo.todoId !== state.todoId)
+
+        return {
+            ...state,
+            todos: [...newTodos, updatedTodo]
+        }
+    }
+}
 
 const TodoContext = ({ children }) => {
 
-    const [todosState, setTodosState] = useState({
+    const [todosState, todoDispatchAction] = useReducer(todoReducerFunction, {
         todoId: undefined,
         todos: [],
     })
 
+    // const [todosState, setTodosState] = useState({
+    //     todoId: undefined,
+    //     todos: [],
+    // })
+
 
     function handelStartAddTodo() {
-        setTodosState(prevState => {
-            return {
-                ...prevState,
-                todoId: null
-            };
-        });
-
-
+        todoDispatchAction({
+            type: "START_ADD_TODO"
+        })
     }
     const handelAddTodo = (todo) => {
+        todoDispatchAction({
+            type: "ADD_TODO",
+            payload: todo
+        })
 
-        setTodosState(prevState => {
-            const id = Math.random();
-            const newTodo = {
-                ...todo,
-                todoId: id,
-                steps: []
-            };
-            return {
-                ...prevState,
-                todoId: undefined,
-                todos: [...prevState.todos, newTodo]
-            };
-        });
     };
 
     const handelcancelTodo = () => {
-        setTodosState(prevState => {
-            return {
-                ...prevState,
-                todoId: undefined
-            };
-        });
+        todoDispatchAction({
+            type: "CANCEL_TODO"
+        })
+
     }
     const handelSelectTodo = (id) => {
-        setTodosState(prevState => {
-            return {
-                ...prevState,
-                todoId: id
-            }
+        todoDispatchAction({
+            type: "SELECTED_TODO",
+            payload: id
         })
+
     }
     const handelAddStep = (step) => {
 
-        setTodosState(prevState => {
-            let newTodos = [...prevState.todos]
-            const todo = newTodos.find(todo => todo.todoId === prevState.todoId)
-            const updatedTodo = { ...todo, steps: [...todo.steps, step] }
-            newTodos = newTodos.filter(todo => todo.todoId !== prevState.todoId)
-
-            return {
-                ...prevState,
-                todos: [...newTodos, updatedTodo]
-            }
+        todoDispatchAction({
+            type: "ADD_STEP",
+            payload: step
         })
+
+
     }
 
     const handelDeleteStep = (stepId) => {
-
-        setTodosState(prevState => {
-            let newTodos = [...prevState.todos]
-            const todo = newTodos.find(todo => todo.todoId === prevState.todoId)
-            const newSteps = todo.steps.filter(step => step.stepId !== stepId)
-            const updatedTodo = { ...todo, steps: newSteps }
-            newTodos = newTodos.filter(todo => todo.todoId !== prevState.todoId)
-
-            return {
-                ...prevState,
-                todos: [...newTodos, updatedTodo]
-            }
+        todoDispatchAction({
+            type: "DELETE_STEP",
+            payload: stepId
         })
+
     }
 
 
